027e51e588e81c85669dc84c2cbbd7582dfa8757
Gabriel Ginn Suárez <ginnlaiho@gmail.com>
2021-02-05 10:23:18 -0600

    - Rspec: Fix two failing tests for the Comments and the Posts validations.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   validates :content, presence: true, length: { maximum: 200,
-                                                too_long: '200 characters in comment is the maximum allowed.' }
+                                                too_long: 'Only 1000 characters per post ' }
 
   belongs_to :user
   belongs_to :post
